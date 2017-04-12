090ed8d36f60200a31eb766bee3af7db5bc85184
Andres <andres70p32@gmail.com>
2017-04-12 08:26:10 +0000

    add env port to deploy on heroku

--- app.js
+++ app.js
@@ -46,6 +46,6 @@ app.get('/api/latest/imagesearch/', (req, res)=>{
         })
 })
 
-app.listen(8080, ()=>{
-    console.log("listening on port 8080")
+app.listen(process.env.PORT || 8080, ()=>{
+    console.log("listening on port: " + process.env.PORT || "8080")
 })
