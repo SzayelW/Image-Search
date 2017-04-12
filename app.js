910fac0e0255108ea828819e795ae6a388a8bb54
Andres <andres70p32@gmail.com>
2017-04-12 08:09:01 +0000

    add new route

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ var mongodb =  require('mongodb').MongoClient
 var express =  require('express')
 var request = require('request')
 var app = express()
-
+var mongoUrl = process.env.MONGOLAB_URI
 var apikey = process.env.API_KEY
 var cx = process.env.CX
 
-app.get('/:search', (req,res)=>{
+app.get('/api/imagesearch/:search', (req,res)=>{
     var search = req.params.search
     var offset = req.query.offset || 10
-    
     var endPoint = "https://www.googleapis.com/customsearch/v1?q="+search+"&cx="+cx+"&num="+offset+"&searchType=image&key="+apikey
+    
+    mongodb.connect(mongoUrl, (err, db)=>{
+        if(err){ throw err}
+            var collection = db.collection('search')
+            var date = new Date()
+            collection.insert({term : search, when: date.toUTCString() })
+    })
+    
     request(endPoint, (error, response, body)=> {
           if (!error && response.statusCode === 200) {
                 const searchResponse = JSON.parse(body)
@@ -25,6 +32,17 @@ app.get('/:search', (req,res)=>{
               res.send({error: "invalid search"})
               console.log("Got an error: ", error, ", status code: ", response.statusCode)
           }
+        })
+})
+
+app.get('/api/latest/imagesearch/', (req, res)=>{
+        mongodb.connect(mongoUrl, (err, db)=>{
+            if(err){throw err}
+            var collection = db.collection('search')
+            collection.find().sort({_id:-1}).limit(10).toArray((err,doc)=>{
+                if(err){throw err}
+                res.json(doc)                
+            })
         })
 })
 
