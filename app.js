294af57158af2297c5c6755554d88285bb2a311c
Andres <andres70p32@gmail.com>
2017-04-12 07:31:39 +0000

    seach images working

--- app.js
+++ app.js
@@ -13,9 +13,14 @@ app.get('/:search', (req,res)=>{
     var endPoint = "https://www.googleapis.com/customsearch/v1?q="+search+"&cx="+cx+"&num="+offset+"&searchType=image&key="+apikey
     request(endPoint, (error, response, body)=> {
           if (!error && response.statusCode === 200) {
-            const searchResponse = JSON.parse(body)
-            console.log("Got a response: ", searchResponse.items)
-                res.send(searchResponse.items)
+                const searchResponse = JSON.parse(body)
+                var items = searchResponse.items
+                var imagesObject = [];
+                items.forEach((item)=>{
+                    var newitem = {url: item.link, snippet: item.snippet , thumbnail: item.image.thumbnailLink, context: item.image.contextLink}
+                    imagesObject.push(newitem)                    
+                })
+                res.json(imagesObject)
           } else {
               res.send({error: "invalid search"})
               console.log("Got an error: ", error, ", status code: ", response.statusCode)
